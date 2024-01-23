b4995e5b7740063f50d418a82dda4d9803642738
Scott Feichter <scottfeichter@gmail.com>
2024-01-23 13:20:25 -0800

    04 finished

--- problems/04-range.js
+++ problems/04-range.js
@@ -10,20 +10,40 @@ range(3, 4); // [3]
 range(7, 6); // []
 ***********************************************************************/
 
-function range(start, end) {
-  debugger;
-  let arr = [];
-  if (end < start) {
-    return arr;
-  } else if (start === end) {
-    return arr;
+// function range(start, end) {
+//   debugger;
+//   let arr = [];
+//   if (end < start) {
+//     return arr;
+//   } else if (start === end) {
+//     return arr;
+//   }
+
+
+//   arr.push(start);
+//   start++;
+
+
+//   return arr.concat(range(start, end));
+// }
+
+
+
+let range = (start, end, array = []) => {
+  if (start > end) {
+    return array;
   }
-
-  arr.push(start);
-  start++;
-
-  return arr.concat(range(start, end));
+  if (start === end) {
+    return array;
+  }
+  if (start < end) {
+    array.push(start);
+  }
+  return range(start + 1, end, array);
 }
+
+
+
 
 console.log(range(1, 5)); // [1, 2, 3, 4])
 console.log(range(3, 4)); // [3]
